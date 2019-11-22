b3140eeb995e5a7d0e1eb66b352d565bf18d3702
郑左璋 <zuozhang.zheng@jihuiduo.cn>
2019-11-22 22:59:31 +0800

    Initial Commit

---
+++ data-analysis-eureka-registry/src/main/java/com/metu/dataanalysiseurekaregistry/DataAnalysisEurekaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.metu.dataanalysiseurekaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalysisEurekaRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalysisEurekaRegistryApplication.class, args);
+    }
+
+}
---
+++ data-analysis-eureka-registry/src/test/java/com/metu/dataanalysiseurekaregistry/DataAnalysisEurekaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metu.dataanalysiseurekaregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalysisEurekaRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
