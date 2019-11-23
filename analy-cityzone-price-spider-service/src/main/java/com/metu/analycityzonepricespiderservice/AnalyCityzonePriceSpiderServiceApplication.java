352a92ac1d486a65bc1fd607d797462fc759e643
郑左璋 <zuozhang.zheng@jihuiduo.cn>
2019-11-23 19:50:08 +0800

    初始版本,爬虫服务

---
+++ analy-cityzone-price-spider-service/src/main/java/com/metu/analycityzonepricespiderservice/AnalyCityzonePriceSpiderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.metu.analycityzonepricespiderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyCityzonePriceSpiderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyCityzonePriceSpiderServiceApplication.class, args);
+    }
+
+}
---
+++ analy-cityzone-price-spider-service/src/test/java/com/metu/analycityzonepricespiderservice/AnalyCityzonePriceSpiderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metu.analycityzonepricespiderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyCityzonePriceSpiderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
