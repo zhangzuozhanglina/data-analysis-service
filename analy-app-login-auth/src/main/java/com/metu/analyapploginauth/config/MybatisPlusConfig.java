a82a556687d6fe5deac6df4e34cbe279421101c6
郑左璋 <zuozhang.zheng@jihuiduo.cn>
2019-11-26 23:06:49 +0800

    配置类

---
+++ analy-app-login-auth/src/main/java/com/metu/analyapploginauth/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.metu.analyapploginauth.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName MybatisPlusConfig
+ * @Description TODO
+ * @Author admin
+ * @Date 2019/11/25 21:59
+ * @Version 1.0
+ **/
+@MapperScan("com.metu.analyapploginauth.mapper")
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
