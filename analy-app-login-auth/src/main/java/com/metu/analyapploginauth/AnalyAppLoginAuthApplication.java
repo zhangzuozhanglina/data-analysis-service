3c96630da9c63989b81e5d5bf14692cc72720c52
郑左璋 <zuozhang.zheng@jihuiduo.cn>
2019-11-28 23:50:22 +0800

    程序入口类

--- analy-app-login-auth/src/main/java/com/metu/analyapploginauth/AnalyAppLoginAuthApplication.java
+++ analy-app-login-auth/src/main/java/com/metu/analyapploginauth/AnalyAppLoginAuthApplication.java
@@ -1,11 +1,18 @@
 package com.metu.analyapploginauth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan(basePackages = {"com"})
+//@MapperScan("com.metu.analyapploginauth.mapper")
 public class AnalyAppLoginAuthApplication {
 
     public static void main(String[] args) {
